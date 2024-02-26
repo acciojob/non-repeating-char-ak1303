8dbc9c92c624ed057893b0a6ea0ee560b834082b
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 00:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str) { 
  // Write your code here
+	for(let i of str){
+   if(str.indexOf(i)==str.lastIndexOf(i))return i;
+ }
+ return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
